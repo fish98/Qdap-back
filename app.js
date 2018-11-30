cf38bcecdb8fe7503881ad32b8f7fbc76437ef04
fish98 <jiongchiyu@gmail.com>
2018-11-30 20:10:52 +0800

    fix database and start connecting

--- app.js
+++ app.js
@@ -4,16 +4,70 @@ const Router = require('koa-router')
 const koaCors = require('@koa/cors')
 const mysql = require('mysql')
 
+// config
+
+const fish = {
+    "id": "fish",
+    "package": "Computer", 
+    "place": "Bird",
+    "deadline": 3,
+    "reward": 5000,
+    "detail": "hahahahahahahaha",
+    "remard": "ttfish",
+    "image": "xxx.jpg" 
+}
+
+const table = "test"
+
+// SQL
+
 let connection = mysql.createConnection({
     host: 'localhost',
+    user: 'root',
+
 })
+
+connection.connect(function(err){
+        if (err) {
+    console.error('error connecting: ' + err.stack)
+    return
+        }
+    })
+
+const findData = (id) => {
+    connection.query(`select * from ${table} where id = ?`, id, (err, result, field) => {
+        if (err) throw err
+        console.log("result:", result)
+        })
+    }
+
+const insertData = (data) => {
+    connection.query(`INSERT INTO ${table} SET ?`, data, (err, result, field) => {
+            if (err) throw err
+            console.log(`result:`)
+        })
+    }
+
+// Middleware
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/new', (ctx, next) => {
-    ctx.body = "Hi TTfish"
-    console.log(ctx.request.body)
+router.get('/all', (ctx) => {
+    ctx.body = ""
+})
+
+// ready to finish feature!
+
+// router.get('/status', (ctx, next) => {
+//     ctx.body = "Hi TTfish"
+//     console.log(ctx.request.body)
+// })
+
+router.post('/new', (ctx) => {
+    ctx.body = "Success"
+    insertData(ctx.request.body)
+    console.log(`insert data ${ctx.request.body}`)
 })
 
 app.use(koaBody())
