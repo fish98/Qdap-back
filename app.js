d5079cc72ad95c35ff0fe493e4885924247c388f
fish98 <jiongchiyu@gmail.com>
2018-11-30 16:27:55 +0800

    remove

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const mysql = require('mysql')
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    // user: 'root',
-    // password: '123456',
-    // database: 'ttfish'
 })
 
 const app = new Koa()
